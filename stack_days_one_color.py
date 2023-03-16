1749ed8f7fb1d1d423cf253b5d51f4009fa25f1c
Rasmus Othar <rasmusothar@gmail.com>
2023-03-16 21:23:54 +0100

    now exports plot to picturefiles, and csv files

--- stack_days_one_color.py
+++ stack_days_one_color.py
@@ -7,12 +7,23 @@ import datapunkter
 
 data_telemetry = apidata.get_data()
 
+
+def sanitize_filename(filename):
+    return ''.join(c for c in filename if c.isalnum() or c == '_')
+
+
 for i, field in datapunkter.TELEMETRY_FIELDS.items():
     print(field)
     name = field["name"]
     human_text = field["human_text"]
     df = formatdata.format_data(data_telemetry, name)
     plotdata.plot_data(df, human_text)
+
+    filename = sanitize_filename(human_text) + '.png'
+    plt.savefig(filename)
+
+    filename = sanitize_filename(human_text) + '.csv'
+    df.to_csv(filename)
 
 
 plt.show()
